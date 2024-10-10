2eef507704c6304fe3e38b39b8004d2156a03ba5
ssingh <satinder.singh@cyber.gc.ca>
2024-10-10 17:30:52 -0400

    file logging

--- utilities/spark.py
+++ utilities/spark.py
@@ -64,6 +64,8 @@ def read_data(spark, file_cfg, input_files):
         # databricks library
         if not file_cfg["xml_row_tag"]:
             raise ValueError("For XML format, 'xml_row_tag' must be provided.")
+
+        logging.info(f"xml_row_tag: {file_cfg['xml_row_tag']}")
         df = (
             spark.read.format("xml")
             .option("rowTag", file_cfg["xml_row_tag"])

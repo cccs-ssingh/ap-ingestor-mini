ba18240afd26fbb8c031c6084b9ed7c711d77ffc
ssingh <satinder.singh@cyber.gc.ca>
2024-11-07 10:10:14 -0500

    matchin EH nvd casting

--- custom_ingestors/nvd.py
+++ custom_ingestors/nvd.py
@@ -1,10 +1,12 @@
 import logging
-from pyspark.sql.functions import col, to_json
+from pyspark.sql.functions import *
 
 
 def apply_custom_rules(df):
-    logging.info(f' - formatting type -> column')
-    for column_name in ['configurations', 'cveTags', 'metrics']:
-        logging.info(f' - StringType() -> {column_name}')
-        df = df.withColumn(column_name, to_json(col(column_name)))
+    # logging.info(f' - formatting type -> column')
+    # for column_name in ['configurations', 'cveTags', 'metrics']:
+    #     logging.info(f' - StringType() -> {column_name}')
+    #     df = df.withColumn(column_name, to_json(col(column_name)))
+    df = df.withColumn("cveTags", from_json(col("cveTags").cast("string"), ArrayType(StringType(), True)))
+
     return df

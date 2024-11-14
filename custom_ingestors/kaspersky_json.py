59cdde2dd3c5a9a00617ae41e6b8adccaee120cb
ssingh <satinder.singh@cyber.gc.ca>
2024-11-14 17:32:46 -0500

    iocs.definition.Indicator.IndicatorItem

--- custom_ingestors/kaspersky_json.py
+++ custom_ingestors/kaspersky_json.py
@@ -5,6 +5,6 @@ from pyspark.sql.functions import *
 def apply_custom_rules(df):
     logging.info(f" - formatting column: 'iocs' -> StringType()")
 
-    df = df.withColumn("definition.Indicator.IndicatorItem", to_json(col("definition.Indicator.IndicatorItem")))
+    df = df.withColumn("iocs.definition.Indicator.IndicatorItem", to_json(col("iocs.definition.Indicator.IndicatorItem")))
 
     return df

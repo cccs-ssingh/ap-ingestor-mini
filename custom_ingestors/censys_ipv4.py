b17e53479e2dfdf6bc69b42bbf1831d37eea8212
Mubin Qureshi <mubin.qureshi@cse-cst.gc.ca>
2024-11-14 15:42:15 -0500

    add condition to check if table exists

--- custom_ingestors/censys_ipv4.py
+++ custom_ingestors/censys_ipv4.py
@@ -5,7 +5,7 @@ def apply_custom_rules(df, spark):
     spark_sql_cmds = [
         "DROP TABLE IF EXISTS hogwarts_u.test.censys_ipv4 PURGE",
         "USE hogwarts_u.test",
-        "ALTER TABLE test.censys_ipv4_stg RENAME TO test.censys_ipv4",
+        "ALTER TABLE IF EXISTS test.censys_ipv4_stg RENAME TO test.censys_ipv4",
         "SHOW TABLES IN hogwarts_u.test LIKE 'censys%'"
     ]
     for cmd in spark_sql_cmds:

43e139c052cf6998533f836e7bcd88fcee135290
Mubin Qureshi <mubin.qureshi@cse-cst.gc.ca>
2024-12-12 15:36:39 -0500

    ingest files to iceberg one at a time

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utilities.config import *
 from utilities.azure import determine_files_to_process
 from utilities.spark import create_spark_session
 from utilities.iceberg import ingest_to_iceberg
-
+import logging
 # Main function
 def run(*args, **kwargs):
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -14,11 +14,14 @@ def run(*args, **kwargs):
     # Determine files tp process from Azure
     files_to_process = determine_files_to_process(cfg['azure'], cfg['file'])
 
+
     # Create Spark session
     spark = create_spark_session(cfg['iceberg']['table']['name'])
 
-    # Ingest files into Iceberg table
-    ingest_to_iceberg(cfg['iceberg'], cfg['file'], spark, files_to_process)
+    for file in files_to_process:
+        logging.info(f"ingesting file to iceberg: {file}")
+        # Ingest files into Iceberg table
+        ingest_to_iceberg(cfg['iceberg'], cfg['file'], spark, [file])
 
 if __name__ == "__main__":
     run()

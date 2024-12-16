f66d38c469db6dd0d60fabdb7ac59ec4cfdf093c
ssingh <satinder.singh@cyber.gc.ca>
2024-12-16 12:10:57 -0500

    match EH

---
+++ custom_ingestors/dragos_indicators.py
@@ -0,0 +1,13 @@
+import pyspark.sql.functions as f
+
+
+def apply_custom_rules(df):
+    #  The objects column is an array of structs.
+    #  We need to explode it to then be able to select all of its inner columns.
+    #  After that, we can filter the type
+
+    df = df.select(f.explode(f.col("objects")).alias("objects"))
+    df = df.select("objects.*")
+    df = df.filter(f.col("type") == "indicator")
+
+    return df

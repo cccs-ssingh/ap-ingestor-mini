db2b496996c2e770faad197fc949fb916188c081
cccs-rpatel <radhey.patel@cyber.gc.ca>
2024-12-18 19:13:10 +0000

    greynoise_noise: fix column datatype

--- custom_ingestors/greynoise_noise.py
+++ custom_ingestors/greynoise_noise.py
@@ -1,12 +1,20 @@
 from pyspark.sql.functions import col, from_utc_timestamp, date_format
 
+
 def apply_custom_rules(df):
+    # Assuming we want to work with the first element of the 'temporal_data' array:
     df = df.withColumn(
-        "raw_data.temporal_data.element.window.end", 
+        "raw_data.temporal_data.element.window.end",
         date_format(
-            from_utc_timestamp(col("raw_data.temporal_data.element.window.end"), "America/Toronto"),  
-            "yyyy-MM-dd HH:mm:ss"  
-        )
+            from_utc_timestamp(
+                col("raw_data.temporal_data")
+                .getItem(0)
+                .getField("window")
+                .getField("end"),
+                "America/Toronto",
+            ),
+            "yyyy-MM-dd HH:mm:ss",
+        ),
     )
 
     return df

c8623361d0497017528bdd9ffd0229f9f9b8a824
cccs-rpatel <radhey.patel@cyber.gc.ca>
2024-12-19 15:34:15 +0000

    greynoise_noise: fix timestamp columns

--- custom_ingestors/greynoise_noise.py
+++ custom_ingestors/greynoise_noise.py
@@ -1,4 +1,4 @@
-from pyspark.sql.functions import col, to_timestamp, explode
+from pyspark.sql.functions import col, to_timestamp, explode, struct
 
 
 def apply_custom_rules(df):
@@ -7,10 +7,23 @@ def apply_custom_rules(df):
     )
 
     df_transformed = df_exploded.withColumn(
-        "raw_data.temporal_data.element.window.end",
+        "temporal_data_exploded_window_end",
         to_timestamp(col("temporal_data_exploded.window.end")),
     )
 
-    df_final = df_transformed.drop("temporal_data_exploded")
+    df_final = df_transformed.withColumn(
+        "raw_data.temporal_data",
+        struct(
+            col("temporal_data_exploded.scan").alias("scan"),
+            struct(
+                col("temporal_data_exploded_window_end").alias("end"),
+                col("temporal_data_exploded.window.start").alias("start"),
+            ).alias("window"),
+        ),
+    )
+
+    df_final = df_final.drop(
+        "temporal_data_exploded", "temporal_data_exploded_window_end"
+    )
 
     return df_final

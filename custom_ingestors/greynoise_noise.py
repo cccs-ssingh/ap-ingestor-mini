72de3b2b673f065410bdb37b88e8415d0b1471d2
ssingh <satinder.singh@cyber.gc.ca>
2025-01-15 14:24:30 -0500

    cast raw_data.temporal_data -> new column raw_data.temporal_data_str

--- custom_ingestors/greynoise_noise.py
+++ custom_ingestors/greynoise_noise.py
@@ -1,13 +1,19 @@
-from pyspark.sql.functions import col, when
+import logging
+
+from pyspark.sql.functions import col, to_json
 
 def apply_custom_rules(df):
 
+    old_col = "raw_data.temporal_data"
+    new_col = "raw_data.temporal_data_str"
+
+    logging.info(f"casting column: {old_col} -> string with new col name: {new_col}")
+
     df = df.withColumn(
-        "raw_data.temporal_data_str",
-        when(
-            col("raw_data.temporal_data").isNotNull(),
-            col("raw_data.temporal_data").cast("string")
-        ).otherwise(None)
+        new_col,  # New column name
+        to_json(
+            col(old_col) # Convert the 'temporal_data' to a JSON string
+        )
     )
 
     return df

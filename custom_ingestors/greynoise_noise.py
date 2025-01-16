a09cdcf3b255e2ad44385218a68249219488192b
ssingh <satinder.singh@cyber.gc.ca>
2025-01-16 12:37:27 -0500

    cast raw_data.temporal_data -> new column raw_data.temporal_data_str

--- custom_ingestors/greynoise_noise.py
+++ custom_ingestors/greynoise_noise.py
@@ -13,15 +13,12 @@ def apply_custom_rules(df):
     df = df.withColumn(
         "raw_data",  # Modify the raw_data struct
         F.struct(
-            F.col("raw_data.hassh"),  # Keep existing fields
-            F.col("raw_data.ja3"),  # Keep existing fields
-            F.col("raw_data.scan"),  # Keep existing fields
-            # Conditionally apply to_json if temporal_data is a struct/array
-            F.when(
-                F.col("raw_data.temporal_data").cast("string").isNotNull(),
-                F.col("raw_data.temporal_data")  # Keep as string if it's already a string
-            ).otherwise(F.to_json(F.col("raw_data.temporal_data"))).alias("temporal_data_str"),
-            F.col("raw_data.web")  # Keep existing fields
+            # Keep existing fields
+            F.col("raw_data.hassh"),
+            F.col("raw_data.ja3"),
+            F.col("raw_data.scan"),
+            F.col("raw_data.web"),
+            F.col("raw_data.temporal_data").alias("temporal_data_str"),
         )
     )
 
